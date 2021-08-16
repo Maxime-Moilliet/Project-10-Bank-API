656ae23e84e9bc6034040937bf21339e46834d40
Maxime-Moilliet <mlltmax66@gmail.com>
2021-08-16 15:39:17 +0200

    feature: create NavBar component

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home } from '../screens/Home';
+import { NavBar } from '../components/NavBar';
 
 export class Routes extends React.Component {
     render() {
         return (
+            <>
+            <NavBar />
+
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
+            </>
         );
     }
 }
