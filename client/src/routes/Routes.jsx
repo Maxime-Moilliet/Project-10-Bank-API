e60a6f5e93a529b76ec5d3df9d51994355cf9fac
Maxime-Moilliet <mlltmax66@gmail.com>
2021-08-16 15:41:24 +0200

    feature: create Footer component

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home } from '../screens/Home';
 import { NavBar } from '../components/NavBar';
+import { Footer } from '../components/Footer';
 
 export class Routes extends React.Component {
     render() {
@@ -12,6 +13,8 @@ export class Routes extends React.Component {
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
+
+            <Footer />
             </>
         );
     }
